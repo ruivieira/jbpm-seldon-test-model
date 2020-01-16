c0f1bfcdf75be31b3160126ce2ff80f443b05fa6
Rui Vieira <ruidevieira@googlemail.com>
2020-01-16 13:58:23 +0000

    Change from regressor to classifier

--- train.py
+++ train.py
@@ -1,38 +1,40 @@
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.externals import joblib
 import random
 
 if __name__=="__main__":
     data = []
 
-    for i in range(10000):
-        r = random.random()
-        data.append({'ActorId': 'john', 'level': 5, 'approved': 0 if r < 0.9 else 1})
+    prices = np.random.normal(loc=1500.0, scale=40.0, size=3000)
+    print(prices)
+    for price in prices:
+        data.append({'brand': 'Lenovo', 'price': price, 'approved': 1})
 
-        v = random.random()
-        data.append({'ActorId': 'mary', 'level': 5, 'approved': 1 if v < 0.9 else 0})
+    prices = np.random.normal(loc=2500.0, scale=40.0, size=3000)
+    for price in prices:
+        data.append({'brand': 'Apple', 'price': price, 'approved': 1})
+        data.append({'brand': 'Lenovo', 'price': price, 'approved': 0})
 
     df = pd.DataFrame(data)
-    df.ActorId = pd.Categorical(pd.factorize(df.ActorId)[0])
-    df.level = pd.Categorical(pd.factorize(df.level)[0])
+    df.brand = pd.Categorical(pd.factorize(df.brand)[0])
     df.approved = pd.Categorical(pd.factorize(df.approved)[0])
 
 
     # Split data
     outputs = df['approved']
-    inputs = df[['ActorId', 'level']]
+    inputs = df[['brand', 'price']]
 
     # split dataset
     X_train, X_test, y_train, y_test = train_test_split(inputs, outputs, test_size=0.4,
                                                         random_state=23)
 
     # Random forest model
-    rf = RandomForestRegressor()
-    pipeline = Pipeline([("regressor", rf)])
+    rf = RandomForestClassifier()
+    pipeline = Pipeline([("classifier", rf)])
     pipeline.fit(X_train, y_train)
 
     print('Model trained!')
@@ -42,3 +44,4 @@ if __name__=="__main__":
     joblib.dump(pipeline, filename_p)
     print('Model saved!')
 
+    model = joblib.load('model.sav')
